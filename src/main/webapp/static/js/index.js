c9204b4bbc5274f0ae4dc35e898b258ec440cc13
heyanjing <1366162208@qq.com>
2018-01-08 22:09:44 +0800

    init

--- src/main/webapp/static/js/index.js
+++ src/main/webapp/static/js/index.js
@@ -2,3 +2,8 @@
  * Created by heyanjing on 2017/12/18 8:38.
  */
 console.log(111);
+
+var str="persons[0][name][a]=heyanjing";
+console.log(str);
+console.log(str.replace(/(\[(\D+)])/,".$2"));
+console.log(str.replace(/(\[(\D+)])/gi,".$2"));
